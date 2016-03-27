ef516d321bd55afc47713f3ff7b83f455e2f438b
Kouji Matsui <k@kekyo.net>
2016-03-27 14:58:26 +0900

    Add dnxcore50 sample

---
+++ dnxcore50-sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CenterCLR.Sgml;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static async Task<int> MainAsync(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var stream = await httpClient.GetStreamAsync("http://www.kekyo.net/"))
+                {
+                    var document = SgmlReader.Parse(stream);
+                    
+                    Console.WriteLine(document);
+                    
+                    return 0;
+                }
+            }
+        }
+        public static int Main(string[] args)
+        {
+            return MainAsync(args).Result;
+        }
+    }
+}
